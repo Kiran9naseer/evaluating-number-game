f54cbdb5562ef247134cf4cc1e5e02847cf765c5
Kiran9naseer <164726031+Kiran9naseer@users.noreply.github.com>
2024-05-24 16:42:59 +0500

    my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+let guess = 20;
+let target = 20;
+if (guess > target) {
+    console.log("your number is too heigh");
+}
+else if (guess < target) {
+    console.log("your guess is too low");
+}
+else {
+    console.log("your guess is correct");
+}
---
+++ app.ts
@@ -0,0 +1,11 @@
+let guess:number = 17
+let target:number = 20
+if(guess > target){
+    console.log("your number is too heigh")
+}
+else if(guess < target){
+    console.log("your guess is too low")
+}
+else{
+    console.log("your guess is correct!")
+}
